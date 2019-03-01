6baaba1fa67bb1eb03ba04698dea7eb388b42c9f
Patrick Cole <hpcmobiledev@gmail.com>
2019-03-01 15:12:15 -0500

    Uses sample data to switch based on active index state
    * a11y improvements
    * Quick-n-dirty sample data to get the idea across.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,52 @@ import './App.css';
 // establish the context we will use throughout the app:
 const IndexContext = createContext({});
 
+// sample data:
+const usersData = [
+  { name: "Aa", category: 65 },
+  { name: "ga", category: 71 },
+  { name: "zd", category: 90 },
+  { name: "de", category: 68 },
+  { name: "kj", category: 75 },
+  { name: "zm", category: 90 },
+  { name: "ab", category: 65 },
+  { name: "pa", category: 80 },
+  { name: "ae", category: 65 },
+  { name: "ei", category: 69 },
+  { name: "us", category: 85 },
+  { name: "ad", category: 65 },
+  { name: "gh", category: 71 }
+];
+
+function UserList( {items} ) {
+
+  const { category } = useContext(IndexContext);
+
+  let activeItems = items.filter( item => item.category === category );
+  return (
+    <ul>
+      {
+        activeItems.length === 0 ?
+          <li>No users available in this index</li>
+        :
+        activeItems.map( (item, index) => {
+          return (
+            <li key={index}>{item.name}</li>
+          )
+        })
+      }
+    </ul>
+  )
+}
+
 function Index( {id} ) {
 
   const { category, updateIndex } = useContext(IndexContext);
   let handleIndexClick = (e) => updateIndex(id);
   return (
-    <li onClick={handleIndexClick} className={ category === id ? "item item--active" : "item"}>
+    <button onClick={handleIndexClick} className={ category === id ? "item item--active" : "item"}>
       {String.fromCharCode(id)}
-    </li>
+    </button>
   );
 }
 
@@ -42,6 +80,8 @@ function App() {
       <h1>Staff Directory</h1>
       <IndexContext.Provider value={{ category:index, updateIndex: onIndexUpdate }}>
         <IndexList />
+        <hr />
+        <UserList items={usersData} />
       </IndexContext.Provider>
     </main>
     
