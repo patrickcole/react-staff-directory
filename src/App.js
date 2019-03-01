cad4a8c99cca6aa65beb94b0e5b5e520512321eb
Patrick Cole <hpcmobiledev@gmail.com>
2019-03-01 14:03:29 -0500

    Builds list A-Z of indicies, also uses context
    * List items are built using for-loop
    * useState is mechanism for changing index
    * useContext is implemented to check each <Index /> for active state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useContext, useState } from 'react';
 import './App.css';
+import { IndexContext } from './IndexContext';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function Index( {id} ) {
+
+  const { category } = useContext(IndexContext);
+
+  return (
+      category === id ? 
+        <li><strong>{String.fromCharCode(id)}</strong></li>
+      : 
+        <li>{String.fromCharCode(id)}</li>
+  )
+}
+
+function IndexList() {
+
+  let generateIndices = () => {
+    let items = [];
+    for ( let n = 65; n <= 90; n++ ) { 
+      items.push( <Index key={n} id={n} /> );
+    }
+    return items;
+  };
+  return generateIndices();
+}
+
+function App() {
+
+  const [index, setIndex] = useState(65);
+
+  const onTestUpdate = (e) => setIndex(80);
+
+  return (
+    <main>
+      <h1>Staff Directory</h1>
+      <IndexContext.Provider value={{ category:index }}>
+        <IndexList />
+      </IndexContext.Provider>
+      <hr />
+      <button onClick={onTestUpdate}>Set to 80</button>
+    </main>
+    
+  )
 }
 
 export default App;
