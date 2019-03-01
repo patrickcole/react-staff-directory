1e8a941939ce4e20930d0747c53c9a57b3eeffe6
Patrick Cole <hpcmobiledev@gmail.com>
2019-03-01 14:24:38 -0500

    Properly uses context to handle state for <Index />
    * Various clean up/refactor actions completed.
    * useContext now utilized with click actions on <Index /> components.
    * CSS styles have been started.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
-import React, { useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import './App.css';
-import { IndexContext } from './IndexContext';
+
+// establish the context we will use throughout the app:
+const IndexContext = createContext({});
 
 function Index( {id} ) {
 
-  const { category } = useContext(IndexContext);
-
+  const { category, updateIndex } = useContext(IndexContext);
+  let handleIndexClick = (e) => updateIndex(id);
   return (
-      category === id ? 
-        <li><strong>{String.fromCharCode(id)}</strong></li>
-      : 
-        <li>{String.fromCharCode(id)}</li>
-  )
+    <li onClick={handleIndexClick} className={ category === id ? "item item--active" : "item"}>
+      {String.fromCharCode(id)}
+    </li>
+  );
 }
 
 function IndexList() {
 
-  let generateIndices = () => {
+  let buildIndices = () => {
     let items = [];
     for ( let n = 65; n <= 90; n++ ) { 
       items.push( <Index key={n} id={n} /> );
     }
     return items;
   };
-  return generateIndices();
+
+  return (
+    <ul className="items">
+      { buildIndices() }
+    </ul>
+  )
 }
 
 function App() {
 
   const [index, setIndex] = useState(65);
-
-  const onTestUpdate = (e) => setIndex(80);
+  const onIndexUpdate = (index) => setIndex(index);
 
   return (
     <main>
       <h1>Staff Directory</h1>
-      <IndexContext.Provider value={{ category:index }}>
+      <IndexContext.Provider value={{ category:index, updateIndex: onIndexUpdate }}>
         <IndexList />
       </IndexContext.Provider>
-      <hr />
-      <button onClick={onTestUpdate}>Set to 80</button>
     </main>
     
   )
